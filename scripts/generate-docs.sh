a81d62f0938f0885dfbcdb630a07caf4d7abdfa9
Demid <you@example.com>
2025-03-21 16:46:41 +0700

    add file sum.py and new directory to docs

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -6,7 +6,7 @@ pip install lazydocs
 # Генерация документации
 lazydocs \
   --output-path="./docs/docstrings" \
-  --overview-file="README.md" \
+  --overview-file="../README.md" \
   --src-base-url="https://github.com/Demid00/access_via_token/blob/master/" \
   src/
 
---
+++ sum.py
@@ -0,0 +1,3 @@
+def sum(a,b):
+    """Get two numbers and return sum of them"""
+    return a + b
